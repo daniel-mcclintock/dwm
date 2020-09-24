1b6417c6dde70ad2f1110175ed36bbe3b5e4fa11
daniel mcclintock <daniel.mcclintock@impactdata.com.au>
2020-09-24 20:30:09 +1000

    Adjust font

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hermit:size=9" };
-static const char dmenufont[]       = "Hermit:size=9";
+static const char *fonts[]          = { "JetBrains Mono:size=11" };
+static const char dmenufont[]       = "JetBrains Mono:size=11";
 /*
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
